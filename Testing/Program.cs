1cfe89f086052af89fdba5587e8fb29b7e199a75
satishpatel2009 <62432137+satishpatel2009@users.noreply.github.com>
2020-03-22 17:20:27 -0400

    Added new method

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -8,9 +8,15 @@ namespace Testing
         {
             Console.WriteLine("Hello World!");
             Program.Print("Git Test");
+            Program.Print1("Git Test1");
         }
 
         static void Print(string test)
+        {
+            Console.WriteLine(test);
+        }
+
+        static void Print1(string test)
         {
             Console.WriteLine(test);
         }
